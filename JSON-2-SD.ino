c3e6aa74bbbaa60785533138231965935b1d94f0
Samuel <samjavaloyes96@gmail.com>
2019-07-16 23:38:27 +0200

    add/write in file

--- JSON-2-SD.ino
+++ JSON-2-SD.ino
@@ -1,43 +1,68 @@
+
+/*
+Circuit for mega del tucuplant: (depende de la placa usa un SPI u otro)
+ * analog sensors on analog ins 0, 1, and 2
+ * SD card attached to SPI bus as follows:
+ ** MOSI - pin 11 - 51
+ ** MISO - pin 12 - 50
+ ** CLK - pin 13 - 52 (SCK)
+ ** CS - pin 4 - 53(for MKRZero SD: SDCARD_SS_PIN)
+ */
+#include <ArduinoJson.h>
+#include <SPI.h>
 #include <SD.h>
- 
-File logFile;
- 
-void setup() 
-{
+
+const int chipSelect = 53;
+
+void setup() {
+DynamicJsonDocument doc(2048);
+
+
   Serial.begin(9600);
-  Serial.print(F("Iniciando SD ..."));
-  if (!SD.begin(9))
-  {
-    Serial.println(F("Error al iniciar"));
-    return;
+  while (!Serial) {
+    ; //Esto se hace para evitar fallos con USB nativo
   }
-  Serial.println(F("Iniciado correctamente"));
-}
- 
- 
-// Funcion que simula la lectura de un sensor
-int readSensor()
-{
-   return 0;
-}
- 
-void loop()
-{
-  // Abrir archivo y escribir valor
-  logFile = SD.open("datalog.txt", FILE_WRITE);
+
+
+  Serial.println("Inicializando cortex prefrontal e hipocampo...");
+
+  // Compribamos que hay tarjeta
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Fallido, el cerebro no puede memorizar y va a suspender la oposicion");
+    // Si no la hay lo dejamos haciendo el ganso en un bucle infinito
+    while (1);
+  }
+  Serial.println("Puedo recordar...MATAR HUMANOS.");
+
   
-  if (logFile) { 
-        int value = readSensor;
-        logFile.print("Time(ms)=");
-        logFile.print(millis());
-        logFile.print(", value=");
-        logFile.println(value);
-        
-        logFile.close();
-  
-  } 
+}
+
+void loop() {
+  // make a string for assembling the data to log:
+  String dataString = "";
+
+  // read three sensors and append to the string:
+  for (int analogPin = 0; analogPin < 3; analogPin++) {
+    int sensor = analogRead(analogPin);
+    dataString += String(sensor);
+    if (analogPin < 2) {
+      dataString += ",";
+    }
+  }
+
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+    // print to the serial port too:
+    Serial.println(dataString);
+  }
+  // if the file isn't open, pop up an error:
   else {
-    Serial.println("Error al abrir el archivo");
+    Serial.println("error opening datalog.txt");
   }
-  delay(500);
 }
